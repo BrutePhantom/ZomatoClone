1bd389061904876a728cc5c8408826116b4cac7f
Sarth <sarthdhut@Sarths-MacBook-Pro.local>
2026-04-25 22:51:12 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Simple footer component shown at the very bottom
+function Footer() {
+  return (
+    <div className="footerDiv">
+      <p>© 2026 Zomato Clone by Student</p>
+    </div>
+  );
+}
+
+export default Footer;
